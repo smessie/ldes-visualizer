d86d6bc6c3f70434705dbedcd5ccfe63ddbf8a34
smessie <smessie@smessie.com>
2025-01-28 08:56:54 +0100

    feat: Install leaflet

---
+++ src/components/MapVisualizer.vue
@@ -0,0 +1,36 @@
+<template>
+    <div style="height: 600px; width: 800px">
+        <l-map ref="map" :zoom="zoom" :center="[47.41322, -1.219482]">
+            <l-tile-layer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                layer-type="base"
+                name="OpenStreetMap"
+            ></l-tile-layer>
+        </l-map>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+export default defineComponent({
+    name: "MapVisualizer",
+    components: {
+        LMap,
+        LTileLayer,
+    },
+    data() {
+        return {
+            zoom: 2,
+        };
+    },
+});
+</script>
+
+<style scoped>
+#app {
+    height: 100vh;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
+// https://github.com/vue-leaflet/vue-leaflet/issues/278#issuecomment-1445996448
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import L from "leaflet";
 
 createApp(App).mount("#app");
